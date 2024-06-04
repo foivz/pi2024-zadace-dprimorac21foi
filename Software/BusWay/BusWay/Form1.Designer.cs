3ada657dd720b1722a7dfdb2426b9a9ab510deec
dprimorac21 <dprimorac21@student.foi.hr>
2024-06-04 02:40:26 +0200

    Forma za prijavu i validaciju

--- Software/BusWay/BusWay/Form1.Designer.cs
+++ Software/BusWay/BusWay/Form1.Designer.cs
@@ -1,6 +1,6 @@
 ﻿namespace BusWay
 {
-    partial class Form1
+    partial class FrmPrijava
     {
         /// <summary>
         /// Required designer variable.
@@ -28,24 +28,81 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.txtKorisnickoIme = new System.Windows.Forms.TextBox();
+            this.txtLozinka = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.btnPrijava = new System.Windows.Forms.Button();
             this.SuspendLayout();
             // 
-            // textBox1
+            // txtKorisnickoIme
             // 
-            this.textBox1.Location = new System.Drawing.Point(407, 145);
-            this.textBox1.Name = "textBox1";
-            this.textBox1.Size = new System.Drawing.Size(100, 22);
-            this.textBox1.TabIndex = 0;
+            this.txtKorisnickoIme.Location = new System.Drawing.Point(324, 231);
+            this.txtKorisnickoIme.Name = "txtKorisnickoIme";
+            this.txtKorisnickoIme.Size = new System.Drawing.Size(188, 22);
+            this.txtKorisnickoIme.TabIndex = 0;
             // 
-            // Form1
+            // txtLozinka
+            // 
+            this.txtLozinka.Location = new System.Drawing.Point(324, 303);
+            this.txtLozinka.Name = "txtLozinka";
+            this.txtLozinka.Size = new System.Drawing.Size(188, 22);
+            this.txtLozinka.TabIndex = 1;
+            this.txtLozinka.TextChanged += new System.EventHandler(this.textBox2_TextChanged);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(194, 231);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(94, 16);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "Korisničko ime";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(194, 306);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(53, 16);
+            this.label2.TabIndex = 3;
+            this.label2.Text = "Lozinka";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Palatino Linotype", 48F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.label3.Location = new System.Drawing.Point(189, 94);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(341, 108);
+            this.label3.TabIndex = 4;
+            this.label3.Text = "BusWay";
+            // 
+            // btnPrijava
+            // 
+            this.btnPrijava.Location = new System.Drawing.Point(424, 360);
+            this.btnPrijava.Name = "btnPrijava";
+            this.btnPrijava.Size = new System.Drawing.Size(88, 38);
+            this.btnPrijava.TabIndex = 5;
+            this.btnPrijava.Text = "Prijava";
+            this.btnPrijava.UseVisualStyleBackColor = true;
+            this.btnPrijava.Click += new System.EventHandler(this.btnPrijava_Click);
+            // 
+            // FrmPrijava
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(806, 498);
-            this.Controls.Add(this.textBox1);
-            this.Name = "Form1";
+            this.ClientSize = new System.Drawing.Size(726, 516);
+            this.Controls.Add(this.btnPrijava);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.txtLozinka);
+            this.Controls.Add(this.txtKorisnickoIme);
+            this.Name = "FrmPrijava";
             this.Text = "Form1";
+            this.Load += new System.EventHandler(this.FrmPrijava_Load);
             this.ResumeLayout(false);
             this.PerformLayout();
 
@@ -53,7 +110,12 @@
 
         #endregion
 
-        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.TextBox txtKorisnickoIme;
+        private System.Windows.Forms.TextBox txtLozinka;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Button btnPrijava;
     }
 }
 
--- Software/BusWay/BusWay/Form1.cs
+++ Software/BusWay/BusWay/Form1.cs
@@ -7,14 +7,55 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using static System.Windows.Forms.VisualStyles.VisualStyleElement.StartPanel;
 
 namespace BusWay
 {
-    public partial class Form1 : Form
+    public partial class FrmPrijava : Form
     {
-        public Form1()
+        public FrmPrijava()
         {
             InitializeComponent();
+        }
+        string korIme = "Dino";
+        string lozinka = "Foi";
+
+        private void btnPrijava_Click(object sender, EventArgs e)
+        {
+            if (txtKorisnickoIme.Text == "")
+            {
+                MessageBox.Show("Korisničko ime nije uneseno!", "Problem",
+                MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+            else if (txtLozinka.Text == "")
+            {
+                MessageBox.Show("Lozinka nije unesena!", "Problem", MessageBoxButtons.OK,
+                MessageBoxIcon.Error);
+            }
+            else
+            {
+                if (txtKorisnickoIme.Text == korIme && txtLozinka.Text == lozinka)
+                {
+                    MessageBox.Show("Dobrodošli!", "Prijavljeni ste",
+                    MessageBoxButtons.OK, MessageBoxIcon.Information);
+                }
+                else
+                {
+                    MessageBox.Show("Krivi podaci!", "Problem", MessageBoxButtons.OK,
+                    MessageBoxIcon.Error);
+                }
+            }
+        }
+
+
+        private void textBox2_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void FrmPrijava_Load(object sender, EventArgs e)
+        {
+
         }
     }
 }
